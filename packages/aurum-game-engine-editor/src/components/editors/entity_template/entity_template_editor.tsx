08dbdaae805535c341dbc0c6b36f16384cd96463
Alex Grethen <20316112+agex-n7@users.noreply.github.com>
2021-09-19 20:31:54 +0200

    move subscriptions after required variables have been initialized

--- packages/aurum-game-engine-editor/src/components/editors/entity_template/entity_template_editor.tsx
+++ packages/aurum-game-engine-editor/src/components/editors/entity_template/entity_template_editor.tsx
@@ -2,17 +2,17 @@ import { css } from '@emotion/css';
 import { aurumify, currentTheme, PanelComponent, PanelContent, PanelDockBottom, PanelDockLeft, PanelDockRight } from 'aurum-components';
 import { EntityTemplateModel, SceneEntityData } from 'aurum-game-editor-shared';
 import {
-	ArrayDataSource,
-	Aurum,
-	AurumComponentAPI,
-	DataSource,
-	DefaultSwitchCase,
-	dsFilter,
-	dsMap,
-	DuplexDataSource,
-	Renderable,
-	Switch,
-	SwitchCase
+    ArrayDataSource,
+    Aurum,
+    AurumComponentAPI,
+    DataSource,
+    DefaultSwitchCase,
+    dsFilter,
+    dsMap,
+    DuplexDataSource,
+    Renderable,
+    Switch,
+    SwitchCase
 } from 'aurumjs';
 import { ProjectFile } from '../../../models/project_file';
 import { reactifySceneModel, setParentsForSceneModel } from '../../../models/scene_entities/reactive_entities_utils';
@@ -34,179 +34,179 @@ import { EntityTemplateModelReactive } from './entity_template_model';
 export interface EntityTemplateEditorProps extends AbstractEditorProps {}
 
 const style = aurumify([currentTheme], (theme, lifecycleToken) =>
-	aurumify(
-		[theme.fontFamily, theme.baseFontSize, theme.highlightFontColor, theme.themeColor1],
-		(fontFamily, size, highlightFont, color1) => css`
-			background-color: ${color1};
-			font-family: ${fontFamily};
-			font-size: ${size};
-			color: ${highlightFont};
-			overflow: hidden;
+    aurumify(
+        [theme.fontFamily, theme.baseFontSize, theme.highlightFontColor, theme.themeColor1],
+        (fontFamily, size, highlightFont, color1) => css`
+            background-color: ${color1};
+            font-family: ${fontFamily};
+            font-size: ${size};
+            color: ${highlightFont};
+            overflow: hidden;
 
-			width: 100%;
-			height: 100%;
-			flex-direction: column;
-			display: flex;
+            width: 100%;
+            height: 100%;
+            flex-direction: column;
+            display: flex;
 
-			.entity-editor {
-				background-color: ${color1};
-			}
+            .entity-editor {
+                background-color: ${color1};
+            }
 
-			.scene-editor-header {
-				align-items: center;
-				display: flex;
-				margin: 8px;
-			}
+            .scene-editor-header {
+                align-items: center;
+                display: flex;
+                margin: 8px;
+            }
 
-			.text-field {
-				width: 80px;
-			}
+            .text-field {
+                width: 80px;
+            }
 
-			.scene-settings {
-				margin-right: 8px;
-			}
+            .scene-settings {
+                margin-right: 8px;
+            }
 
-			.scene-editor-camera-controls {
-				margin-right: 8px;
-			}
+            .scene-editor-camera-controls {
+                margin-right: 8px;
+            }
 
-			.scene-editor-content {
-				display: flex;
-				width: 100%;
-				height: 100%;
-			}
-		`,
-		lifecycleToken
-	)
+            .scene-editor-content {
+                display: flex;
+                width: 100%;
+                height: 100%;
+            }
+        `,
+        lifecycleToken
+    )
 );
 
 export function EntityTemplateEditor(props: EntityTemplateEditorProps, children: Renderable[], api: AurumComponentAPI) {
-	props.onSuspend?.subscribe(() => {
-		props.input.content.updateUpstream(JSON.stringify(save(modelReactive), undefined, 4));
-	}, api.cancellationToken);
+    const sizeX = new DataSource<number>();
+    const sizeY = new DataSource<number>();
+    const posX = new DuplexDataSource(0);
+    const posY = new DuplexDataSource(0);
+    const zoom = new DuplexDataSource(1);
+    const model: EntityTemplateModel = JSON.parse(props.input.content.value);
+    const modelReactive: EntityTemplateModelReactive = {
+        code: new DuplexDataSource(model.code),
+        entities: setParentsForSceneModel(reactifySceneModel(model.entities))
+    };
 
-	props.onSaveRequested?.subscribe(() => {
-		props.input.content.updateUpstream(JSON.stringify(save(modelReactive), undefined, 4));
-	}, api.cancellationToken);
+    const grid: SceneGrid = {
+        xSpace: new DuplexDataSource<number>(32),
+        ySpace: new DuplexDataSource<number>(32),
+        render: new DuplexDataSource<boolean>(true),
+        color: new DuplexDataSource<string>('#ddd'),
+        snap: new DuplexDataSource<boolean>(false)
+    };
 
-	const sizeX = new DataSource<number>();
-	const sizeY = new DataSource<number>();
-	const posX = new DuplexDataSource(0);
-	const posY = new DuplexDataSource(0);
-	const zoom = new DuplexDataSource(1);
-	const model: EntityTemplateModel = JSON.parse(props.input.content.value);
-	const modelReactive: EntityTemplateModelReactive = {
-		code: new DuplexDataSource(model.code),
-		entities: setParentsForSceneModel(reactifySceneModel(model.entities))
-	};
+    const editTarget = new DataSource<SceneEntityDataReactive>();
+    const codeFile: ProjectFile = {
+        content: modelReactive.code,
+        diskPath: props.input.diskPath,
+        projectPath: props.input.projectPath,
+        type: ProjectExplorerNodeType.Code
+    };
 
-	const grid: SceneGrid = {
-		xSpace: new DuplexDataSource<number>(32),
-		ySpace: new DuplexDataSource<number>(32),
-		render: new DuplexDataSource<boolean>(true),
-		color: new DuplexDataSource<string>('#ddd'),
-		snap: new DuplexDataSource<boolean>(false)
-	};
+    props.onSuspend?.subscribe(() => {
+        props.input.content.updateUpstream(JSON.stringify(save(modelReactive), undefined, 4));
+    }, api.cancellationToken);
 
-	const editTarget = new DataSource<SceneEntityDataReactive>();
-	const codeFile: ProjectFile = {
-		content: modelReactive.code,
-		diskPath: props.input.diskPath,
-		projectPath: props.input.projectPath,
-		type: ProjectExplorerNodeType.Code
-	};
+    props.onSaveRequested?.subscribe(() => {
+        props.input.content.updateUpstream(JSON.stringify(save(modelReactive), undefined, 4));
+    }, api.cancellationToken);
 
-	return (
-		<div class={style}>
-			<div class="scene-editor-header">
-				<CameraControls posX={posX} posY={posY} zoom={zoom}></CameraControls>
-				<GridControls {...grid}></GridControls>
-			</div>
-			<div class="scene-editor-content">
-				<PanelComponent>
-					<PanelDockLeft resizable size={200}>
-						<EntityPicker
-							onEntryDoubleClicked={(entry: EntityTypeTreeNode) => {
-								if (entry) {
-									modelReactive.entities.push(entry.entityFactory());
-								}
-							}}
-						></EntityPicker>
-						<SceneGraphView
-							editTarget={editTarget}
-							rootNodes={[
-								{
-									data: modelReactive.entities,
-									name: 'templateRoot'
-								}
-							]}
-						></SceneGraphView>
-					</PanelDockLeft>
-					<PanelDockBottom resizable size={300}>
-						<CodeEditor input={codeFile} openFile={props.openFile}></CodeEditor>
-					</PanelDockBottom>
-					<PanelDockRight resizable size={300}>
-						<div class="entity-editor">
-							<Switch state={editTarget}>
-								<SwitchCase when={undefined}>No entity selected</SwitchCase>
-								<DefaultSwitchCase>
-									<EntityEditor
-										editTarget={editTarget.transform(
-											dsFilter((e) => !!e),
-											dsMap((entity) => {
-												return sceneEntityToEntityEditor(entity, resolveSchema(entity));
-											})
-										)}
-									></EntityEditor>
-								</DefaultSwitchCase>
-							</Switch>
-						</div>
-					</PanelDockRight>
-					<PanelContent>
-						<ScenePreview
-							grid={grid}
-							editTarget={editTarget}
-							sceneEntities={modelReactive.entities}
-							cameraX={posX}
-							cameraY={posY}
-							allowDrag
-							resolutionX={sizeX}
-							resolutionY={sizeY}
-							zoom={zoom}
-						></ScenePreview>
-					</PanelContent>
-				</PanelComponent>
-			</div>
-		</div>
-	);
+    return (
+        <div class={style}>
+            <div class="scene-editor-header">
+                <CameraControls posX={posX} posY={posY} zoom={zoom}></CameraControls>
+                <GridControls {...grid}></GridControls>
+            </div>
+            <div class="scene-editor-content">
+                <PanelComponent>
+                    <PanelDockLeft resizable size={200}>
+                        <EntityPicker
+                            onEntryDoubleClicked={(entry: EntityTypeTreeNode) => {
+                                if (entry) {
+                                    modelReactive.entities.push(entry.entityFactory());
+                                }
+                            }}
+                        ></EntityPicker>
+                        <SceneGraphView
+                            editTarget={editTarget}
+                            rootNodes={[
+                                {
+                                    data: modelReactive.entities,
+                                    name: 'templateRoot'
+                                }
+                            ]}
+                        ></SceneGraphView>
+                    </PanelDockLeft>
+                    <PanelDockBottom resizable size={300}>
+                        <CodeEditor input={codeFile} openFile={props.openFile}></CodeEditor>
+                    </PanelDockBottom>
+                    <PanelDockRight resizable size={300}>
+                        <div class="entity-editor">
+                            <Switch state={editTarget}>
+                                <SwitchCase when={undefined}>No entity selected</SwitchCase>
+                                <DefaultSwitchCase>
+                                    <EntityEditor
+                                        editTarget={editTarget.transform(
+                                            dsFilter((e) => !!e),
+                                            dsMap((entity) => {
+                                                return sceneEntityToEntityEditor(entity, resolveSchema(entity));
+                                            })
+                                        )}
+                                    ></EntityEditor>
+                                </DefaultSwitchCase>
+                            </Switch>
+                        </div>
+                    </PanelDockRight>
+                    <PanelContent>
+                        <ScenePreview
+                            grid={grid}
+                            editTarget={editTarget}
+                            sceneEntities={modelReactive.entities}
+                            cameraX={posX}
+                            cameraY={posY}
+                            allowDrag
+                            resolutionX={sizeX}
+                            resolutionY={sizeY}
+                            zoom={zoom}
+                        ></ScenePreview>
+                    </PanelContent>
+                </PanelComponent>
+            </div>
+        </div>
+    );
 }
 
 export interface SceneRendererProps {
-	selected: DataSource<SceneEntityDataReactive>;
-	dragSession: DataSource<DragSession>;
-	model: ArrayDataSource<SceneEntityDataReactive>;
+    selected: DataSource<SceneEntityDataReactive>;
+    dragSession: DataSource<DragSession>;
+    model: ArrayDataSource<SceneEntityDataReactive>;
 }
 
 function save(modelReactive: EntityTemplateModelReactive): EntityTemplateModel {
-	return {
-		code: modelReactive.code.value,
-		entities: saveEntities(modelReactive.entities)
-	};
+    return {
+        code: modelReactive.code.value,
+        entities: saveEntities(modelReactive.entities)
+    };
 }
 
 function saveEntities(entities: ArrayDataSource<SceneEntityDataReactive>): SceneEntityData[] {
-	return entities.getData().map((s) => {
-		const staticProps = {};
-		for (const key in s.properties) {
-			staticProps[key] = s.properties[key].value;
-		}
+    return entities.getData().map((s) => {
+        const staticProps = {};
+        for (const key in s.properties) {
+            staticProps[key] = s.properties[key].value;
+        }
 
-		return {
-			children: saveEntities(s.children),
-			name: s.name.value,
-			innerText: s.innerText?.value,
-			namespace: s.namespace,
-			properties: staticProps
-		};
-	});
+        return {
+            children: saveEntities(s.children),
+            name: s.name.value,
+            innerText: s.innerText?.value,
+            namespace: s.namespace,
+            properties: staticProps
+        };
+    });
 }
